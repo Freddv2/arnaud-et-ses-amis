403ef709709fcb0b4a8b1cce6d82a5773661a51a
fvincent <fredddv@hotmail.com>
2021-02-23 18:57:05 -0500

    WIP Animals image carousel. Re-added .js config file

--- ui/src/components/Animals.vue
+++ ui/src/components/Animals.vue
@@ -1,12 +1,20 @@
 <template>
-  <Animal
-    :name="currentAnimal.name"
-    :image="currentAnimal.image"
-  />
+  <div class="row">
+    <div class="column">
+      <button
+    </div>
+    <div class="column">
+      <Animal
+          :image="animals[animalIndex].image"
+          :name="animals[animalIndex].name"
+      />
+    </div>
+    <div class="column"></div>
+  </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
 import Animal from "./Animal.vue";
 import * as animalsData from '../data/animals.json'
 
@@ -14,11 +22,26 @@ export default defineComponent({
   name: "Animals",
   components: {Animal},
   setup: () => {
-    const animals = animalsData.animals
-    const currentAnimal = animals[0]
-    console.log(animals)
+    const animals = ref(animalsData.animals)
+    const index = ref(0)
 
-    return {currentAnimal, animals}
+    function nextAnimals() {
+      if (index.value === animals.value.length - 1) {
+        index.value = 0;
+      } else {
+        index.value++
+      }
+    }
+
+    function previousAnimals() {
+      if (index.value === 0) {
+        index.value = animals.value.length - 1;
+      } else {
+        index.value--
+      }
+    }
+
+    return {animals, nextAnimals, previousAnimals}
   }
 })
 </script>
