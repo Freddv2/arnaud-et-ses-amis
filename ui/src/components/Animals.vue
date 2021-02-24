e80fa1804eb8f142929c003e72ad36ea3e95d45b
fvincent <fredddv@hotmail.com>
2021-02-24 09:06:13 -0500

    WIP implementing image carousel

--- ui/src/components/Animals.vue
+++ ui/src/components/Animals.vue
@@ -1,15 +1,33 @@
 <template>
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <Animal
+            :image="animals[animalIndex].image"
+            :name="animals[animalIndex].name"
+        />
+      </div>
+      <a class="carousel-control-prev" data-slide="prev" href="#carouselExampleControls" role="button" @click.prevent="prev">
+        <span aria-hidden="true" class="carousel-control-prev-icon"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" data-slide="next" href="#carouselExampleControls" role="button" @click.prevent="next">
+        <span aria-hidden="true" class="carousel-control-next-icon"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+  </div>
   <div class="row">
     <div class="column">
-      <button
+      <button @click="previousAnimals"/>
     </div>
     <div class="column">
-      <Animal
-          :image="animals[animalIndex].image"
-          :name="animals[animalIndex].name"
-      />
+
     </div>
-    <div class="column"></div>
+    <div class="column">
+      <button @click="nextAnimals"/>
+    </div>
   </div>
 </template>
 
@@ -22,26 +40,38 @@ export default defineComponent({
   name: "Animals",
   components: {Animal},
   setup: () => {
+    //add the active property for the carousel. Set the first animal to active
+    animalsData.animals.forEach((el, i) => el.active = i === 0)
     const animals = ref(animalsData.animals)
-    const index = ref(0)
+    let directionClass = ref('')
 
-    function nextAnimals() {
-      if (index.value === animals.value.length - 1) {
-        index.value = 0;
-      } else {
-        index.value++
-      }
+    function getActiveIndex() {
+      return animals.value.findIndex(el => el.active)
     }
 
-    function previousAnimals() {
-      if (index.value === 0) {
-        index.value = animals.value.length - 1;
-      } else {
-        index.value--
+    function prev() {
+      const currentActiveIndex = getActiveIndex()
+      let newActiveIndex = currentActiveIndex - 1
+      if (newActiveIndex < 0) {
+        newActiveIndex = animals.value.length - 1;
       }
+      animals.value[currentActiveIndex].active = false
+      animals.value[newActiveIndex].active = true
+      directionClass.value = 'slide-prev';
     }
 
-    return {animals, nextAnimals, previousAnimals}
+    function next() {
+      const currentActiveIndex = getActiveIndex()
+      let newActiveIndex = currentActiveIndex + 1
+      if (newActiveIndex > animals.value.length - 1) {
+        newActiveIndex = 0;
+      }
+      animals.value[currentActiveIndex].active = false
+      animals.value[newActiveIndex].active = true
+      directionClass.value = 'slide-next';
+    }
+
+    return {animals, prev, next}
   }
 })
 </script>
